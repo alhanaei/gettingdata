cfb625a0bf36b481798fbcd7d417c0be86c6be5c
alhanaei@hotmail.com <alhanaei@hotmail.com>
2015-02-22 13:01:29 +0400

    Initial commit

---
+++ run_analysis.R
@@ -0,0 +1,59 @@
+
+library("plyr")
+##directory location for the data
+directory1 <-"C:/Users/aaalhanaee/Documents/DataScience/Coursera/UCI HAR/train"
+directory2 <-"C:/Users/aaalhanaee/Documents/DataScience/Coursera/UCI HAR/test"
+directory3 <-"C:/Users/aaalhanaee/Documents/DataScience/Coursera/UCI HAR"
+
+
+## read training data
+file.list <-list.files(path = directory1,full.names = TRUE,pattern = "*.txt")
+train.subject <-read.csv(file.list[1],sep = "",header=FALSE)
+train.x<-read.csv(file.list[2],sep = "",header=FALSE)
+train.y<-read.csv(file.list[3],sep = "",header=FALSE)
+
+##read test data
+file.list <-list.files(path = directory2,full.names = TRUE,pattern = "*.txt")
+test.subject <-read.csv(file.list[1],sep = "",header=FALSE)
+test.x<-read.csv(file.list[2],sep = "",header=FALSE)
+test.y<-read.csv(file.list[3],sep = "",header=FALSE)
+
+#read activity and subject data
+file.list <-list.files(path = directory3,full.names = TRUE,pattern = "*.txt")
+activity<-read.csv(file.list[1],sep = "",header=FALSE)
+features <-read.csv(file.list[2],sep = "",header=FALSE)
+
+
+#combine test and training data
+xdata<-rbind(test.x,train.x)
+ydata<-rbind(test.y,train.y)
+subjectdata <- rbind(test.subject,train.subject)
+
+##transform activity in activity names
+activitydata <- join(ydata,activity)
+
+##compining all data into one data set
+alldata<-cbind(subjectdata,activitydata$V2,xdata)
+
+
+## changing the column names
+dcoln<-c("Subject","Activity",as.vector(features$V2[]))
+dcoln<-gsub("-"," ",dcoln)
+dcoln<-gsub("[()]","",dcoln)
+tolower(dcoln)
+colnames(alldata)<-c(dcoln)
+
+
+## selecting mean and SD columns
+selectedcol <- c(1,2,grep(".*mean.*|.*std.*",colnames(alldata)))
+untidy<-alldata[selectedcol]
+
+
+## generating the Tidy data
+tidy<-aggregate(x = untidy[-(1:2)], by = list(newdata$Subject,newdata$Activity), FUN = "mean")
+
+## saving the tidy data to a text file.
+write.table(tidy,"tidy.txt", row.name=FALSE)
+
+    
+    
